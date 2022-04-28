3806ef87feaef667562b30aa56cbe68734049e07
Kirsten <kirsten@Kirstens-MacBook-Pro.local>
2022-04-28 14:19:40 -0400

    startup.sh added

--- app.js
+++ app.js
@@ -31,9 +31,8 @@ const pokemons = require('./public/data/pokedex.json')
 // *********************************************************** //
 
 const mongoose = require( 'mongoose' );
-//const mongodb_URI = 'mongodb://localhost:27017/cs103a_todo'
-const mongodb_URI = 'mongodb+srv://cs103a:Spring2022@cluster0.hb11m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-
+const mongodb_URI = 'mongodb://localhost:27017/cs103a_todo'
+// const mongodb_URI = process.env.mongodb_URI
 
 mongoose.connect( mongodb_URI, { useNewUrlParser: true, useUnifiedTopology: true } );
 // fix deprecation warnings 
@@ -144,7 +143,7 @@ app.post('/pokemon/byName',
 app.get('/pokemon/byName/:name',
   // show a list of Pokemons by a given type
   async (req,res,next) => {
-    const name = req.params.name
+    const {name} = req.params
     const pokemon = await Pokemon.findOne({ name : {$regex: name, $options: 'i'}})
     res.locals.pokemon = pokemon
     res.render('pokemon')
