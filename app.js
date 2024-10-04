bd658e090f5add531910b41ff2843fa59cefbdd2
ktapalla <tenjaeqt@gmail.com>
2024-10-04 14:23:50 -0700

    heroku

--- app.js
+++ app.js
@@ -34,8 +34,8 @@ const pokemons = require('./public/data/pokedex.json')
 // *********************************************************** //
 
 const mongoose = require( 'mongoose' );
-const mongodb_URI = 'mongodb://localhost:27017/cs103a_todo'
-// const mongodb_URI = process.env.mongodb_URI
+// const mongodb_URI = 'mongodb://localhost:27017/cs103a_todo'
+const mongodb_URI = process.env.mongodb_URI
 
 
 mongoose.connect( mongodb_URI, { useNewUrlParser: true, useUnifiedTopology: true } );
